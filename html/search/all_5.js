6cc2983907ca17fa5382d9f11d9ba4d1621bcfad
darwincsg <63426856+darwincsg@users.noreply.github.com>
2022-01-30 23:09:19 +0000

    Add files via upload

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['m_0',['M',['../functions-team-8_8c.html#a52037c938e3c1b126c6277da5ca689d0',1,'M():&#160;functions-team-8.c'],['../functions-team-8_8h.html#a52037c938e3c1b126c6277da5ca689d0',1,'M():&#160;functions-team-8.h']]],
-  ['menu_5fajuda_1',['menu_ajuda',['../functions-team-8_8h.html#a0dd9114b9439f836b9d76e6b01a8a9a0',1,'functions-team-8.h']]],
-  ['menu_5fprincipal_2',['menu_principal',['../functions-team-8_8h.html#ae3a600065cfe3e0105b6209e35d20264',1,'functions-team-8.h']]],
+  ['menu_5fajuda_1',['menu_ajuda',['../functions-team-8_8c.html#a0dd9114b9439f836b9d76e6b01a8a9a0',1,'menu_ajuda():&#160;functions-team-8.c'],['../functions-team-8_8h.html#a0dd9114b9439f836b9d76e6b01a8a9a0',1,'menu_ajuda():&#160;functions-team-8.c']]],
+  ['menu_5fprincipal_2',['menu_principal',['../functions-team-8_8c.html#ae3a600065cfe3e0105b6209e35d20264',1,'menu_principal():&#160;functions-team-8.c'],['../functions-team-8_8h.html#ae3a600065cfe3e0105b6209e35d20264',1,'menu_principal():&#160;functions-team-8.c']]],
   ['metade1metade2_3',['metade1metade2',['../functions-team-8_8c.html#aedc73ebefd74eb2c6c3adefa810f704f',1,'metade1metade2(int arrayOfValues[N]):&#160;functions-team-8.c'],['../functions-team-8_8h.html#aedc73ebefd74eb2c6c3adefa810f704f',1,'metade1metade2(int arrayOfValues[N]):&#160;functions-team-8.c']]],
   ['minimomultiplocomum_4',['minimoMultiploComum',['../functions-team-8_8c.html#a0f1426145e33c57633a24a748b48ec45',1,'minimoMultiploComum(int arrayofValues[N]):&#160;functions-team-8.c'],['../functions-team-8_8h.html#a0f1426145e33c57633a24a748b48ec45',1,'minimoMultiploComum(int arrayofValues[N]):&#160;functions-team-8.c']]],
   ['mostrarvetor_5',['mostrarVetor',['../functions-team-8_8c.html#a578fd7e0c097ed54cbc2bd544921b8ba',1,'mostrarVetor(int arrayOfValues[N]):&#160;functions-team-8.c'],['../functions-team-8_8h.html#a578fd7e0c097ed54cbc2bd544921b8ba',1,'mostrarVetor(int arrayOfValues[N]):&#160;functions-team-8.c']]],
